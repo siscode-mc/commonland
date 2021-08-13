fb43572a595ca3a99f2cfc769d5df83014c9cf5c
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-13 15:37:15 -0300

    Implements WorldManager.java

--- src/main/java/commonland/core/WorldManager.java
+++ src/main/java/commonland/core/WorldManager.java
@@ -1,4 +1,40 @@
 package commonland.core;
 
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
+
+import java.util.*;
+
 public class WorldManager {
+    // this boi maps (ChunkPos -> Claim UUID)
+    // for a simple way to check if a certain chunk is already claimed
+    private Map<ChunkPos, UUID> position = new HashMap<>();
+
+    // this boi maps (Player UUID -> Set<Claim UUID>)
+    // for a simple way to check if a player is part of a claim
+    // and which claims this player are a part of
+    private Map<UUID, Set<UUID>> membership = new HashMap<>();
+
+    private Map<UUID, Claim> claims = new HashMap<>();
+
+    private final ServerWorld world;
+
+    public WorldManager(ServerWorld world) {
+        this.world = world;
+    }
+
+    public boolean isPartOf(UUID player, UUID claim) {
+        if(!membership.containsKey(player)) registerNewMember(player);
+        return membership.get(player).contains(claim);
+    }
+
+    public boolean isClaimed(ChunkPos pos) {
+        return position.containsKey(pos);
+    }
+
+    private void registerNewMember(UUID player) {
+        membership.put(player, new HashSet<>());
+        //TODO: some IO madness for data persistency
+    }
+
 }
