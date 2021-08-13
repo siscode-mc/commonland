7b3c4b66da70635051d66979f3a53faf550cf48d
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-13 15:03:41 -0300

    Implement Claim.java

--- src/main/java/commonland/core/Claim.java
+++ src/main/java/commonland/core/Claim.java
@@ -1,4 +1,43 @@
 package commonland.core;
 
+import java.util.List;
+import java.util.UUID;
+
 public abstract class Claim {
+    private final UUID claimId;
+    private List<UUID> members;
+    private final UUID owner;
+    private String name;
+
+    protected Claim(UUID claimId, List<UUID> members, UUID owner, String name) {
+        this.claimId = claimId;
+        this.members = members;
+        this.owner = owner;
+        this.name = name;
+    }
+
+    public UUID getClaimId() {
+        return claimId;
+    }
+
+    public List<UUID> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<UUID> members) {
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getOwner() {
+        return owner;
+    }
+
 }
