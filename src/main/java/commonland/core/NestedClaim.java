015a3779410d88454bb5da2ca240cc43d5a998aa
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-13 15:10:34 -0300

    Implement NestedClaim.java

--- src/main/java/commonland/core/NestedClaim.java
+++ src/main/java/commonland/core/NestedClaim.java
@@ -1,4 +1,22 @@
 package commonland.core;
 
-public class NestedClaim {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class NestedClaim extends Claim {
+    private List<Claim> subclaims = new ArrayList<>();
+
+    protected NestedClaim(UUID claimId, List<UUID> members, UUID owner, String name) {
+        super(claimId, members, owner, name);
+    }
+
+    public void addSubclaim(Claim claim) {
+        subclaims.add(claim);
+    }
+
+    public void removeSubclaim(Claim claim) {
+        if(!subclaims.contains(claim)) return;
+        subclaims.remove(claim);
+    }
 }
