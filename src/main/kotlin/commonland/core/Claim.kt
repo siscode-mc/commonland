a777eefb4a488d8db55d901be8c8dd17dc1e366d
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-14 12:52:39 -0300

    Add properties to Claim

--- src/main/kotlin/commonland/core/Claim.kt
+++ src/main/kotlin/commonland/core/Claim.kt
@@ -1,4 +1,30 @@
 package commonland.core
 
-class Claim {
+import commonland.core.components.ClaimComponent
+import commonland.core.containers.NaiveSpacialList
+import commonland.core.containers.emptySpacialContainer
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import java.util.*
+
+class Claim (val parent : Claim?, val owner : UUID) : Space {
+    val members: Set<UUID> = mutableSetOf()
+    val children: NaiveSpacialList<Claim> = emptySpacialContainer()
+    val components: NaiveSpacialList<ClaimComponent> = emptySpacialContainer()
+
+    override val boundingBox: Box
+        get() = TODO("Not yet implemented")
+
+    override fun contains(pos: BlockPos): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun overlaps(aabb: Box): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun overlaps(other: Space): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
