ce8e19a7767f6116bc3705e08cdb2ac73a473ae9
wffirilat <wffirilat@users.noreply.github.com>
2021-08-14 12:36:33 -0500

    Add documentation about the need to specialise Space.overlaps

--- src/main/kotlin/commonland/core/Space.kt
+++ src/main/kotlin/commonland/core/Space.kt
@@ -7,15 +7,25 @@ import net.minecraft.util.math.Box
 interface Space {
     val boundingBox : Box
     fun contains(pos : BlockPos) : Boolean
-    fun contains(aabb: Box) : Boolean
 
+    fun contains(aabb: Box) : Boolean
     fun contains(space: Space) : Boolean
+
+    /**
+     * This default implementation works in general, but is very slow.
+     * it should be specialised as much as possible
+     */
     fun overlaps(aabb: Box) : Boolean {
         for (pos in this.boundingBox.intersection(aabb).iterBlocks()) {
             if (this.contains(pos)) return true
         }
         return false
     }
+
+    /**
+     * This default implementation works in general, but is very slow.
+     * it should be specialised as much as possible
+     */
     fun overlaps(other: Space) : Boolean {
         for (pos in this.boundingBox.intersection(other.boundingBox).iterBlocks()) {
             if (this.contains(pos) && other.contains(pos)) return true
