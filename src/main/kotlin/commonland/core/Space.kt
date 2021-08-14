0ef5269f4e9c25afcd09dc627e3ea32a17e5b1d1
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-14 14:38:52 -0300

    Add contains overrides for Box and Space

--- src/main/kotlin/commonland/core/Space.kt
+++ src/main/kotlin/commonland/core/Space.kt
@@ -6,6 +6,8 @@ import net.minecraft.util.math.Box
 interface Space {
     val boundingBox : Box
     fun contains(pos : BlockPos) : Boolean
+    fun contains(aabb: Box) : Boolean
+    fun contains(space: Space) : Boolean
     fun overlaps(aabb: Box) : Boolean
     fun overlaps(other: Space) : Boolean
 }
