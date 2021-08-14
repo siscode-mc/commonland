2a34210c5c7798cee38f975cc22fdafeea5eef19
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-14 12:48:44 -0300

    Add Box.combine function

--- src/main/kotlin/commonland/utils/AABB.kt
+++ src/main/kotlin/commonland/utils/AABB.kt
@@ -1,2 +1,14 @@
 package commonland.utils
 
+import net.minecraft.util.math.Box
+import kotlin.math.max
+import kotlin.math.min
+
+fun Box.combine(other: Box) = Box(
+    min(this.minX, other.minX),
+    min(this.minY, other.minY),
+    min(this.minZ, other.minZ),
+    max(this.maxX, other.maxX),
+    max(this.maxY, other.maxY),
+    max(this.maxZ, other.maxZ)
+)
