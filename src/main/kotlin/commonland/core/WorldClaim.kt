385ad1a5a33c107bfbaa0f545098c7200a3aa1a0
roridev <t3ctotalmenterandom1@outlook.com>
2021-08-15 19:37:57 -0300

    Add WorldClaim.kt

---
+++ src/main/kotlin/commonland/core/WorldClaim.kt
@@ -0,0 +1,22 @@
+package commonland.core
+
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.world.World
+import java.util.*
+
+class WorldClaim(owner: UUID, val world: World) : AbstractClaim(owner) {
+    override fun notifyChildChanged(child: Claim) {
+        TODO("Not yet implemented")
+    }
+
+    override val boundingBox: Box
+        get() = Box(-30_000_000.0,
+                    world.bottomY+0.0,
+                    -30_000_000.0,
+                    30_000_000.0,
+                    world.topY+0.0,
+                    30_000_000.0)
+
+    override fun contains(pos: BlockPos): Boolean = true
+}
