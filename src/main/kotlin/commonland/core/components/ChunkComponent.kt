8c2c9a41a3ec80916f1d44006441d5976fa26c0d
roridev <t3ctotalmenterandom1@outlook.com>
2021-10-04 02:33:21 -0300

    Implement ChunkComponent.kt.kt

--- src/main/kotlin/commonland/core/components/ChunkComponent.kt
+++ src/main/kotlin/commonland/core/components/ChunkComponent.kt
@@ -1,4 +1,23 @@
 package commonland.core.components
 
-class ChunkComponent {
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.ChunkPos
+
+class ChunkComponent(val chunk : ChunkPos) : ClaimComponent() {
+    override val boundingBox: Box
+        get() = Box((chunk.x * 16).toDouble()
+                    , Double.MIN_VALUE
+                    , (chunk.z * 16).toDouble()
+                    , ((chunk.x + 1) * 16).toDouble()
+                    , Double.MAX_VALUE
+                    , ((chunk.z + 1) * 16).toDouble()
+        )
+
+    override fun contains(pos: BlockPos): Boolean {
+        val chunkX = pos.x / 16
+        val chunkZ = pos.z / 16
+
+        return chunk.x == chunkX && chunk.z == chunkZ
+    }
 }
