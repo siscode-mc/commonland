1bdd429d3374f6342d1b8501128511f900459b44
roridev <t3ctotalmenterandom1@outlook.com>
2021-10-04 02:10:35 -0300

    Implement CuboidComponent.kt

--- src/main/kotlin/commonland/core/components/CuboidComponent.kt
+++ src/main/kotlin/commonland/core/components/CuboidComponent.kt
@@ -1,4 +1,13 @@
 package commonland.core.components
 
-class CuboidComponent {
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+
+class CuboidComponent(val posA: BlockPos, val posB: BlockPos) : ClaimComponent() {
+    override val boundingBox: Box
+        get() = Box(posA, posB)
+
+    override fun contains(pos: BlockPos): Boolean {
+        return boundingBox.contains(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble())
+    }
 }
